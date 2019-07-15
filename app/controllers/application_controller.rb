c6717749d8daff6beda1e287fd9464a1597879e9
bigfaiber <greatfaiber@gmail.com>
2019-07-14 19:17:31 -0500

    validations, routes and json format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :ensure_json_request  
+  protect_from_forgery unless: -> { request.format.json? }
+
+def ensure_json_request  
+  return if request.format == :json
+  render :nothing => true, :status => 406  
+end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
- 
+  post '/url.json', to: 'urls#url', defaults: {format: :json}
+  
+  get '/top.json', to: 'urls#top100', defaults: {format: :json}
+
+  get '/:encoded_url', to: 'urls#show', defaults: {format: :json}
+
 end
